f46bd90c2c5569265d6482e3edc203cb2eea46c2
swatanabe1122 <151703642+swatanabe1122@users.noreply.github.com>
2023-12-08 14:19:09 +0900

    Issue4~6のレビュー後に、修正後
    ハッシュロケットではなく、シンボル型で記述
    ssue4~6のレビュー後に、さらに修正後

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -43,10 +43,10 @@ class CalendarsController < ApplicationController
       day = Date.today.wday
       
         days = { 
-          :month => (@todays_date + x).month, 
-          :date => (@todays_date + x).day, 
-          :plans => today_plans, 
-          :wday => wdays[wday_num]
+          month: (@todays_date + x).month, 
+          date: (@todays_date + x).day, 
+          plans:  today_plans, 
+          wday:  wdays[wday_num]
         }
       @week_days.push(days)
     end
